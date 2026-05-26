06d756437ce43e1a05636719c4946b5f86693065
Eduardo de Carvalho <eduardofc@ic.ufrj.br>
2026-05-25 22:10:18 -0300

    enhancement(entities): make fields required insted of setting up defaults

--- Domain/Entities/Produto.cs
+++ Domain/Entities/Produto.cs
@@ -1,11 +1,10 @@
 namespace minipdv.Domain.Entities;
-using System.ComponentModel.DataAnnotations;
 
 public class Produto
 {
     public int Id { get; set; }
-    public string Descricao { get; set; } = string.Empty;
-    public bool Ativo { get; set; } = true;
+    public required string Descricao { get; set; }
+    public required bool Ativo { get; set; } = true;
     public DateTime CriadoEm { get; set; }
     public DateTime? AtualizadoEm { get; set; }
 }
--- Domain/Entities/Usuario.cs
+++ Domain/Entities/Usuario.cs
@@ -1,13 +1,12 @@
 namespace minipdv.Domain.Entities;
-using System.ComponentModel.DataAnnotations;
 
 public class Usuario
 {
     public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public string Login { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public bool Ativo { get; set; } = true;
+    public required string Nome { get; set; }
+    public required string Login { get; set; }
+    public required string PasswordHash { get; set; }
+    public required bool Ativo { get; set; } = true;
     public DateTime CriadoEm { get; set; }
     public DateTime? AtualizadoEm { get; set; }
 }
