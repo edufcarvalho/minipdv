2be23d155a71142e6f1122f9073a52d53b336fcd
Eduardo de Carvalho <eduardofc@ic.ufrj.br>
2026-06-01 23:16:21 -0300

    fix(entities): add misisng migration

---
+++ Migrations/20260602021549_AddPosicaoToVendaItem.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace minipdv.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPosicaoToVendaItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VendaItem",
+                table: "VendaItem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Posicao",
+                table: "VendaItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VendaItem",
+                table: "VendaItem",
+                columns: new[] { "VendaId", "ProdutoId", "Posicao" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VendaItem",
+                table: "VendaItem");
+
+            migrationBuilder.DropColumn(
+                name: "Posicao",
+                table: "VendaItem");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VendaItem",
+                table: "VendaItem",
+                columns: new[] { "VendaId", "ProdutoId" });
+        }
+    }
+}
